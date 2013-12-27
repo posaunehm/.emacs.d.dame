45a9d36691c8581088384ca1f6ce7aa5411feb93
Hiroshi Maekawa <posaunehm@gmail.com>
2013-12-28 08:20:41 +0900

    add groovy mode

--- init.el
+++ init.el
@@ -39,3 +39,17 @@
 (autoload 'cmd-mode "cmd-mode" "CMD mode." t)
 (setq auto-mode-alist (append '(("\\.\\(cmd\\|bat\\)$" . cmd-mode))
 			      auto-mode-alist))
+
+;;; turn on syntax highlighting
+(global-font-lock-mode 1)
+
+;;; use groovy-mode when file ends in .groovy or has #!/bin/groovy at start
+(autoload 'groovy-mode "groovy-mode" "Major mode for editing Groovy code." t)
+(add-to-list 'auto-mode-alist '("\.groovy$" . groovy-mode))
+(add-to-list 'interpreter-mode-alist '("groovy" . groovy-mode))
+
+;;; make Groovy mode electric by default.
+(add-hook 'groovy-mode-hook
+          '(lambda ()
+             (require 'groovy-electric)
+             (groovy-electric-mode)))
